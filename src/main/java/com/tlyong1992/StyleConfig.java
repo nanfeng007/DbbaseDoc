6acd3da1da7a7a9e01c296e5eee2f38693828539
zhangben <1003116337@qq.com>
2018-07-16 16:12:19 +0800

    no message

---
+++ src/main/java/com/tlyong1992/StyleConfig.java
@@ -0,0 +1,12 @@
+package com.tlyong1992;
+
+import java.awt.*;
+
+/**
+ * USER：tangly
+ * DATE：2017/10/19
+ * TIME：15:56
+ */
+public class StyleConfig {
+    static Color TABLE_HEAD_COLOR = Color.lightGray;
+}
